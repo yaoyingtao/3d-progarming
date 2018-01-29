edbbfc18d16bd722e5ddac7d1e24fc4c68265f3b
yaoyingtao <yaoyingtao@changba.com>
2018-01-29 17:42:49 +0800

    新建ch1工程

---
+++ Ch1/Ch1/GLView.m
@@ -0,0 +1,20 @@
+//
+//  GLView.m
+//  Ch1
+//
+//  Created by tomy yao on 2018/1/29.
+//  Copyright © 2018年 tomy yao. All rights reserved.
+//
+
+#import "GLView.h"
+
+@implementation GLView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+}
+
+@end
---
+++ Ch1/Ch1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Ch1
+//
+//  Created by tomy yao on 2018/1/29.
+//  Copyright © 2018年 tomy yao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
